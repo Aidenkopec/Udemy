c6bfa0f943b9427bfd4640b365113c1a517e7151
Aidenkopec <Aidenkopec@icloud.com>
2022-05-11 20:35:34 -0600

    update

---
+++ src/Arrays/Intro/sortArray.java
@@ -0,0 +1,21 @@
+package Arrays.Intro;
+
+import java.util.Collection;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class sortArray {
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Integer[] array = new Integer[];
+
+//                {106, 26, 81, 5, 15};
+//        Arrays.sort(array, Comparator.reverseOrder());
+
+
+
+        System.out.println("Array in descending order: " + Arrays.toString(array));
+    }
+
+}
